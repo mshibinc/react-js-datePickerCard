4de04ad11a038f895808b6a61678a66d0750deab
mshibinc <38204529+mshibinc@users.noreply.github.com>
2021-05-10 09:10:59 +0530

    style updated

--- src/components/DatePickerCard.js
+++ src/components/DatePickerCard.js
@@ -14,11 +14,41 @@ const Card = styled.div`
   padding: 0.5rem;
   border-radius: 3rem;
   background: #471a8a;
-  &.react-datepicker {
+  & .react-datepicker {
     border: 0;
     /* font-family:'' */
     background: transparent;
     color: ${rgba(255, 255, 255, 0.85)};
+  }
+  & .react-datepicker__navigation {
+    top: 0.875rem;
+    opacity: 0.5;
+  }
+  & .react-datepicker__header {
+    padding: 0;
+    border: 0;
+    background: transparent;
+  }
+  & .react-datepicker__current-month {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 2.5rem;
+    color: ${rgba(255, 255, 255, 0.85)};
+  }
+  & .react-datepicker__day {
+    outline: none;
+    border-radius: 50%;
+    color: ${rgba(255, 255, 255, 0.5)};
+  }
+  & .react-datepicker__day-name {
+    color: ${rgba(255, 255, 255, 0.5)};
+  }
+  & .react-datepicker__day--selected,
+  & .react-datepicker__day--keyboard-selected,
+  & .react-datepicker__day:hover {
+    background: #311758;
+    color: white;
   }
 `;
 
